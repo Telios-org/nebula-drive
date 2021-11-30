6a78286b7d7a6870af23caa1d72ad31c5d2fe571
hexadecibal <hex#github@telios.io>
2021-11-30 12:55:15 -0500

    Upgrading to latest Autobase/Hypercore

--- tests/autobee.test.js
+++ tests/autobee.test.js
@@ -5,8 +5,9 @@ const Autobee = require('../lib/autobee')
 const ram = require('random-access-memory')
 
 tape('Autobee - create local writer', async t => {
-  const localWriterCore = Hypercore(ram, { server: true, client: true })
-  const base = new Autobase([localWriterCore], { input: localWriterCore })
+  const localOutput= Hypercore(ram, { server: true, client: true })
+  const localInput = Hypercore(ram, { server: true, client: true })
+  const base = new Autobase([localInput], { input: localInput, outputs: localOutput })
   const autobee = new Autobee(base, {
     keyEncoding: 'utf-8',
     valueEncoding: 'json'
@@ -20,9 +21,9 @@ tape('Autobee - create local writer', async t => {
 
 tape('Autobee - create sub', async t => {
   const encryptionKey = Buffer.alloc(32, 'hello world')
-  const localWriterCore = Hypercore(ram, { encryptionKey, server: true, client: true })
-  const indexCore = Hypercore(ram, { encryptionKey })
-  const base = new Autobase([localWriterCore], { input: localWriterCore, indexes: indexCore })
+  const localInput = Hypercore(ram, { encryptionKey, server: true, client: true })
+  const localOutput = Hypercore(ram, { encryptionKey })
+  const base = new Autobase([localInput], { input: localInput, outputs: localOutput })
   const autobee = new Autobee(base, {
     keyEncoding: 'utf-8', // can be set to undefined (binary), utf-8, ascii or and abstract-encoding
     valueEncoding: 'json'
