5e01c9ff26da733f3c3616aef2a6a65bef549990
hexadecibal <hex#github@telios.io>
2021-12-03 10:45:24 -0500

    Reverting hypercore upgrade and only upgrading hyperswarm

--- tests/autobee.test.js
+++ tests/autobee.test.js
@@ -5,9 +5,8 @@ const Autobee = require('../lib/autobee')
 const ram = require('random-access-memory')
 
 tape('Autobee - create local writer', async t => {
-  const localOutput= Hypercore(ram, { server: true, client: true })
-  const localInput = Hypercore(ram, { server: true, client: true })
-  const base = new Autobase([localInput], { input: localInput, outputs: localOutput })
+  const localWriterCore = Hypercore(ram, { server: true, client: true })
+  const base = new Autobase([localWriterCore], { input: localWriterCore })
   const autobee = new Autobee(base, {
     keyEncoding: 'utf-8',
     valueEncoding: 'json'
@@ -21,9 +20,9 @@ tape('Autobee - create local writer', async t => {
 
 tape('Autobee - create sub', async t => {
   const encryptionKey = Buffer.alloc(32, 'hello world')
-  const localInput = Hypercore(ram, { encryptionKey, server: true, client: true })
-  const localOutput = Hypercore(ram, { encryptionKey })
-  const base = new Autobase([localInput], { input: localInput, outputs: localOutput })
+  const localWriterCore = Hypercore(ram, { encryptionKey, server: true, client: true })
+  const indexCore = Hypercore(ram, { encryptionKey })
+  const base = new Autobase([localWriterCore], { input: localWriterCore, indexes: indexCore })
   const autobee = new Autobee(base, {
     keyEncoding: 'utf-8', // can be set to undefined (binary), utf-8, ascii or and abstract-encoding
     valueEncoding: 'json'
